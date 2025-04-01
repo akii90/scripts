1ac1d080aebf09fa6c792441f9e227debfb7f195
vicen <xqfxqf111@gmail.com>
2025-04-01 14:20:36 +0800

    add color info

---
+++ color.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Color define
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+
+# Reset, no color
+NC='\033[0m'
+
+# Color output example
+echo -e "${RED}Red content${NC}"
