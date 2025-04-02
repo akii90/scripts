1f01eb806e863d14980dee16cea9795ce8c5a9fa
vicen <xqfxqf111@gmail.com>
2025-04-02 09:56:23 +0800

    update logs func for general func

--- general_functions.sh
+++ general_functions.sh
@@ -1,22 +1,29 @@
 #! /bin/bash
 
-log_info() {
-    echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') - $1"
+# Logs
+info()
+{
+    echo '[INFO] ' "$@"
+}
+warn()
+{
+    echo '[WARN] ' "$@" >&2
+}
+fatal()
+{
+    echo '[ERROR] ' "$@" >&2
+    exit 1
 }
 
-log_error_then_exit() {
-    echo "[ERROR] $(date '+%Y-%m-%d %H:%M:%S') - $1" && exit 1
-}
-
-create_dir_safe() {
+# Create dir and file 
+create_dir() {
     if [[ ! -d "$1" ]]; then
         mkdir -p "$1" || echo "Could't create dir: $1"
     else
         echo "Dir alread exist: $1"
     fi
 }
-
-create_file_safe() {
+create_file() {
     if [[ ! -f "$1" ]]; then
         touch "$1" || echo "Could't create file: $1"
     else
