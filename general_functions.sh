5e5eb1dcf3a364e29e9e5148bc2f1ab88203a2f0
vicen <xqfxqf111@gmail.com>
2025-04-03 10:29:14 +0800

    add color print func to general functions

--- general_functions.sh
+++ general_functions.sh
@@ -30,3 +30,20 @@ create_file() {
         echo "File alread exist: $1"
     fi
 }
+
+# Color define
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+# Reset, no color
+RESET='\033[0m'
+
+# Color print function
+color_print() {
+    local color=$1
+    local message=$2
+    echo -e "${color}${message}${RESET}"
+}
